15013aaa5598205a0ce2ddc39ca2ad5b0f3e445e
Anderson Dario Arendt <dario@localhost.localdomain>
2021-01-09 20:12:58 -0300

    Update

--- modules/aws/lambda/rest/main.tf
+++ modules/aws/lambda/rest/main.tf
@@ -50,7 +50,7 @@ resource "aws_api_gateway_method" "proxy" {
 
     rest_api_id   = aws_api_gateway_rest_api.api.id
     resource_id   = aws_api_gateway_resource.proxy[each.key].id
-    http_method   = "GET"
+    http_method   = each.value.method
     authorization = "NONE"
 }
 
